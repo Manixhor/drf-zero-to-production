63660717abc38bcc82ebca7e24ecf26c9bfd78c1
MANI <mani@MANIs-MacBook-Air.local>
2026-05-28 22:06:36 +0530

    learnt

--- students/models.py
+++ students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#creation of table 
+
+
+class SampleTable(models.Model):
+    age = models.IntegerField()
+    weight = models.FloatField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    name = models.CharField(max_length=150)
+    feedback = models.TextField()
+    date_of_birth = models.DateField(auto_now=True)
+
+    is_admin = models.BooleanField()
+
+    
