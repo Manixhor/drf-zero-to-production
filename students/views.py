8e51bc8dd04a531a76124900287534723947647d
MANI <mani@MANIs-MacBook-Air.local>
2026-05-29 17:39:47 +0530

    written first api post

--- students/views.py
+++ students/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+
+
+from rest_framework.views  import APIView
+from rest_framework.response import Response
+
+from .models import Student
+
+#here api's can be created as functions or classes 
+
+class StudentAPI(APIView):
+    def post(self,request):
+        print(request.data)
+
+        #to save the api 
+
+        new_student = Student(name=request.data['name'],age=request.data['age'])
+
+        new_student.save()
+
+
+        return Response("Created")
